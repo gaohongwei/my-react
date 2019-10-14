77d2b054dbd6ac15159e20a6b2729b561f7560ff
Gaohong Wei <gaohongwei@gmail.com>
2019-10-14 13:11:29 -0700

    Initial steps, add widgets, bootstrap

---
+++ steps.rb
@@ -0,0 +1,9 @@
+// Initial
+  npx create-react-app my-app
+  npm install -g react-create
+//
+react-create comp Home -d --jsx
+
+// Need final-form
+yarn add final-form react-final-form
+yarn add bootstrap@4.3.1
