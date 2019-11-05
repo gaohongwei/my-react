f7242dab7fe3d2504f904980352b7595f589e6e2
Gaohong Wei <gaohongwei@gmail.com>
2019-11-05 13:59:25 -0800

    reorg ...

--- src/data/DB.js
+++ src/data/DB.js
@@ -15,25 +15,3 @@ export default function getData() {
   return data;
 }
 
-export function getStaticData() {
-  return [
-    {
-      id: 100,
-      name: 'Justin Wei',
-      age: 20,
-      friend: {
-        name: 'Friend1 Maurer',
-        age: 23,
-      }
-    },
-    {
-      id: 200,
-      name: 'Jeffrey Wei',
-      age: 16,
-      friend: {
-        name: 'Friend2 Maurer',
-        age: 23,
-      }
-    },
-  ];
-}
